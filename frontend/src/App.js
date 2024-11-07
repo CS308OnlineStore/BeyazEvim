23996b5a78368a4b8923313bb5b4529c781083cd
canberkilisal <canberkilisal@gmail.com>
2024-11-07 21:31:08 +0300

    fixed navigation problem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import ProductPage from './pages/ProductPage';
+import SignInSignUp from './pages/SignInSignUp';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 const App = () => {
   return (
@@ -10,6 +13,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/signinsignup" element={<SignInSignUp />} />
+        <Route path="/signup" element={<SignIn />} />
+        <Route path="/signin" element={<SignUp />} />
       </Routes>
     </Router>
   );
