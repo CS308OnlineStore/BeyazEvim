ca774a1dc0093f4195b2de480a741924b1043b0f
Öykü Pekgöz <oykupekgoz@Oykus-MacBook-Pro.local>
2024-11-20 20:08:45 +0300

    subcategory name yerine sub category id gosterme sorunu cozuldu

--- frontend/src/pages/SubCategoryPage.js
+++ frontend/src/pages/SubCategoryPage.js
@@ -93,7 +93,7 @@ const SubCategoryPage = () => {
 
       {/* Product List */}
       <div style={productListStyle}>
-        <h1>Products in {subcategory}</h1>
+        <h1>Products in {subcategory.category.name}</h1>
         {loading ? (
           <p>Loading products...</p>
         ) : (
