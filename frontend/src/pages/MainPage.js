cf68452420232e5826cf36b5e576e4bfff0be75d
Öykü Pekgöz <oykupekgoz@Oykus-MacBook-Pro.local>
2024-12-21 17:56:35 +0300

    carousel duzenlendi main pagede

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -29,6 +29,7 @@ import newLogo from '../assets/BeyazEvim_new_logo.jpeg';
 import slide1 from '../assets/slide1.jpg';
 import slide2 from '../assets/slide2.jpg';
 import slide3 from '../assets/slide3.jpg';
+import slide4 from '../assets/slide4.jpg';
 
 
 const { Header, Sider, Content } = Layout;
@@ -210,6 +211,9 @@ const MainPage = () => {
             </div>
             <div>
               <img src={slide3} alt="Slide 3" style={{ width: '100%', height: 'auto' }} />
+            </div>
+            <div>
+              <img src={slide4} alt="Slide 4" style={{ width: '100%', height: 'auto' }} />
             </div>
           </Carousel>
 
