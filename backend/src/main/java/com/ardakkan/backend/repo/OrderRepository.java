f5a038ec3d96556e71ac475c5c057c4768312142
ufukguwenc <148459601+ufukguwenc@users.noreply.github.com>
2025-01-11 01:33:49 +0300

    Revenue ve revenue chart eklendi

--- backend/src/main/java/com/ardakkan/backend/repo/OrderRepository.java
+++ backend/src/main/java/com/ardakkan/backend/repo/OrderRepository.java
@@ -5,6 +5,8 @@ import com.ardakkan.backend.entity.OrderStatus;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +15,5 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     // Belirli bir kullanıcının tüm siparişlerini bulmak
     List<Order> findByUserId(Long userId);
     Optional<Order> findByUserIdAndStatus(Long userId, OrderStatus status);
+    List<Order> findByOrderDateBetween(Date startDate, Date endDate);
 }
